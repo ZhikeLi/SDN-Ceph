221b16d2e04ab67e0bbca0291cd7895c6490b1e7
ZhikeLi <67780125+ZhikeLi@users.noreply.github.com>
2022-09-09 09:58:52 +0800

    Add files via upload

---
+++ ryu_m1.0/Ryu_Controller_Optimize/setting.py
@@ -0,0 +1,7 @@
+SW_PORT=[(1,3),(2,1)]#skip port
+
+MONITOR_PERIOD = 2 # For monitoring traffic
+
+TOSHOW = False  # For showing information in terminal
+
+MAX_CAPACITY = 281 #474976710655L  # Max capacity of link
